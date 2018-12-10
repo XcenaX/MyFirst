3cb3328ae3c37f8fb5bafd84679ec0c9f87bc0d7
XcenaX <40425017+XcenaX@users.noreply.github.com>
2018-12-10 17:00:12 +0600

    Add files via upload

---
+++ onlineShop/onlineShop/Program.cs
@@ -0,0 +1,21 @@
+﻿using onlineShop.Data;
+using onlineShop.Models;
+using System.Linq;
+
+namespace onlineShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new ShopContext())
+            {
+               
+                context.Items.Add(new Item { Name = "Vlad", Price = 100000});
+                context.Users.Add(new User { Name = "vlad", Age = 16 });
+                context.SaveChanges();               
+            }
+            
+        }
+    }
+}
